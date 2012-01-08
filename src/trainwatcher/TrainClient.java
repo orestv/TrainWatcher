0b3f945091521c973f1029d9708f751aa06268bf
Orest Voloshchuk <orest.v@gmail.com>
2012-01-08 23:42:34 +0200

    Working on proper cookie-redirect-grabbing code

--- src/trainwatcher/TrainClient.java
+++ src/trainwatcher/TrainClient.java
@@ -25,24 +25,21 @@ import java.util.logging.Logger;
  * @author ovoloshchuk
  */
 public class TrainClient {
+
     private final static String URL_LOGIN = "http://www.e-kvytok.com.ua/wps/portal/!ut/p/c5/04_SB8K8xLLM9MSSzPy8xBz9CP0os3gjCyNPD0dnAw8DY283A0_XkECDQDNXAwNfE_1wkA6zeAMcwNFA388jPzdVvyA7rxwAhYnqvg!!/dl3/d3/L0lDUWtpQ1NTUW9LVVFBISEvb0lvZ0FFQ1FRREdJUXBURE9DNEpuQSEhLzRDd2lSLXJmbTE2SWt5WGlnRUEhLzdfQ0dBSDQ3TDAwMDY2QzAyQjU2NlI5QTMwTTUvd3BzLnBvcnRsZXRzLmxvZ2lu/";
     //private final static String URL_FIND_STATION = "http://www.e-kvytok.com.ua/wps/myportal/!ut/p/c5/04_SB8K8xLLM9MSSzPy8xBz9CP0os3gDf1MjYzcXYws3A0cvc08LSx8DKADKR2LKmxrD5fHrDgfZh18_WB4HcDTArt8CYYOfR35uqn5BboRBZkC6IgCaRiDN/dl3/d3/L0lDU0lKSWdrbUNTUS9JUFJBQUlpQ2dBek15cXpHWUEhIS80QkVqOG8wRmxHaXQtYlhwQUh0Qi83XzkxSEcxMjgyOE9WSUIwMkZWN1RVSVMyMEcwL3dIMTZ2MTIyMzAwMDMvc2EucmV6ZXJ2YXRpb24uRm9ybUFjdGlvbg!!/#7_91HG12828OVIB02FV7TUIS20G0";
     private final static String URL_FIND_STATION = "http://www.e-kvytok.com.ua/wps/myportal/!ut/p/c5/04_SB8K8xLLM9MSSzPy8xBz9CP0os3gDf1MjYzcXYws3A0cvc08LSx8DKADKR2LKmxrD5fHrDgfZh18_WB4HcDTArt8CYYOfR35uqn6kfpR5vKWhh7uhkYWRhX-Yp5OBkVuYeUioZ7CRgbuBfmROanpicqV-QW6EQWZARiAA_7KIxQ!!/dl3/d3/L0lJSklna21DU1EhIS9JRGpBQU15QUJFUkNKRXFnLzRGR2dzbzBWdnphOUlBOW9JQSEhLzdfOTFIRzEyODI4T1ZJQjAyRlY3VFVJUzIwRzAvUll3dl8zODU3MDAxNS9zYS5yZXplcnZhdGlvbi5Gb3JtQWN0aW9u/";
     private final static String URL_FIND_STATION_BASE = "http://www.e-kvytok.com.ua/wps/myportal/!ut/p/c5/04_SB8K8xLLM9MSSzPy8xBz9CP0os3gDf1MjYzcXYws3A0cvc08LSx8DKADKR2LKmxrD5fHrDgfZh18_WB4HcDTArt8CYYOfR35uqn5BboRBZkC6IgCaRiDN/dl3/d3/L2dJQSEvUUt3QS9ZQnZ3LzZfME81MjNGRDM4RjBBSjdJODhMMDAwMDAwMDA!/";
-    
     private final static String FORM_LOGIN_LOGIN = "wps.portlets.userid";
     private final static String FORM_LOGIN_PASSWORD = "password";
     private final static String FORM_LOGIN_ACTION = "ns_7_CGAH47L00066C02B566R9A30M5__login";
-    
     private final static String FORM_FIND_FROM = "PC_7_91HG12828OVIB02FV7TUIS20G0000000_rezervation.EditFormTAG0";
     private final static String FORM_FIND_ACTION = "PC_7_91HG12828OVIB02FV7TUIS20G0000000_rezervation.SubmitSp1";
-    
     private String sLogin;
     private String sPassword;
     private String sCookies = "";
-    
     private Map<String, String> cookies;
-    
+
     public TrainClient() {
         cookies = new HashMap<String, String>();
     }
@@ -52,32 +49,32 @@ public class TrainClient {
         this.sLogin = sLogin;
         this.sPassword = sPassword;
     }
-    
+
     public boolean login() throws IOException {
         String sParams = formParameters(FORM_LOGIN_LOGIN, sLogin,
                 FORM_LOGIN_PASSWORD, sPassword,
                 FORM_LOGIN_ACTION, "Log in");
         HttpURLConnection conn = request(URL_LOGIN, sParams, false);
-        
+
         return (conn.getResponseCode() == 302);
     }
-    
+
     private static String formParameters(String... strings) {
         StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < strings.length; i += 2){
+
+        for (int i = 0; i < strings.length; i += 2) {
             String sName = strings[i];
-            String sValue = strings[i+1];
+            String sValue = strings[i + 1];
             try {
                 sb.append(sName).append("=").append(URLEncoder.encode(sValue, "utf-8")).append("&");
             } catch (UnsupportedEncodingException ex) {
                 Logger.getLogger(TrainClient.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
         return sb.toString();
     }
-    
+
     public String[] getCities(String sCityName) throws IOException {
         HttpURLConnection conn1 = request(URL_FIND_STATION_BASE, "", true);
         String sParams = formParameters(FORM_FIND_FROM, sCityName,
@@ -90,69 +87,81 @@ public class TrainClient {
         String sResponse = getResponse(conn);
         return null;
     }
-    
-    private HttpURLConnection request(String sUrl, String sParams, boolean bFollowRedirect) throws MalformedURLException, IOException {
+
+    public HttpURLConnection request(String sUrl, String sParams, boolean bFollowRedirect) throws MalformedURLException, IOException {
         HttpURLConnection conn = (HttpURLConnection) new URL(sUrl).openConnection();
-        conn.setRequestMethod("POST");
+        if (sParams != null) {
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Length", Integer.toString(sParams.getBytes().length));
+        } else {
+            conn.setRequestMethod("GET");
+        }
         conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        conn.setRequestProperty("Content-Length", Integer.toString(sParams.getBytes().length));
-        conn.setInstanceFollowRedirects(bFollowRedirect);
-        
+        conn.setInstanceFollowRedirects(false);
+
         conn.setRequestProperty("Accept-Charset", "utf-8");
         conn.setRequestProperty("Cache-Control", "max-age=0");
         conn.addRequestProperty("Connection", "keep-alive");
         conn.setRequestProperty("Host", "www.e-kvytok.com.ua");
         conn.setRequestProperty("Origin", "http://www.e-kvytok.com.ua");
         //conn.setRequestProperty("Referer", sUrl);
-        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7");        
-        
-        if (!cookies.isEmpty())
-            conn.setRequestProperty("Cookie", getCookieLine(cookies));        
-                
-        conn.setUseCaches (false);
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7");
+
+        if (!cookies.isEmpty()) {
+            conn.setRequestProperty("Cookie", getCookieLine(cookies));
+        }
+
+        conn.setUseCaches(false);
         conn.setDoOutput(true);
-        
+
         DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
         dos.writeBytes(sParams);
         dos.flush();
-        dos.close();        
-                        
+        dos.close();
+
         String sHeaderName;
         String sCookie = null;
-        for (int i = 1; (sHeaderName = conn.getHeaderFieldKey(i)) != null; i++)
+        String sLocation = null;
+        for (int i = 1; (sHeaderName = conn.getHeaderFieldKey(i)) != null; i++) {
             if (sHeaderName.equals("Set-Cookie")) {
                 sCookie = conn.getHeaderField(i);
                 String[] split = sCookie.split("=");
                 String sCookieName = split[0], sCookieValue = split[1];
-                cookies.put(sCookieName,  sCookieValue);
+            } else if (sHeaderName.equals("Location")) {
+                sLocation = conn.getHeaderField(i);
             }
-        
-        return conn;        
+        }
+        if (sLocation != null && conn.getResponseCode() == 302) {
+            return request(sLocation, null, true);
+        }
+        return conn;
     }
-    
+
     private static String getResponse(HttpURLConnection conn) throws IOException {
         StringBuilder sb = new StringBuilder();
-        
+
         String sLine;
         InputStream is;
-        if (conn.getErrorStream() == null)
+        if (conn.getErrorStream() == null) {
             is = conn.getInputStream();
-        else
+        } else {
             is = conn.getErrorStream();
-        BufferedReader  rdr = new BufferedReader(new InputStreamReader(is));
-        while ((sLine = rdr.readLine()) != null)
+        }
+        BufferedReader rdr = new BufferedReader(new InputStreamReader(is));
+        while ((sLine = rdr.readLine()) != null) {
             sb.append(sLine);
-        
+        }
+
         return sb.toString();
     }
-    
+
     private static String getCookieLine(Map<String, String> cookies) {
         StringBuilder sb = new StringBuilder();
-        
-        for(String sName : cookies.keySet()) {
+
+        for (String sName : cookies.keySet()) {
             sb.append(String.format("%s=%s", sName, cookies.get(sName)));
         }
-        
+
         return sb.toString();
     }
 }
--- src/trainwatcher/TrainWatcher.java
+++ src/trainwatcher/TrainWatcher.java
@@ -5,6 +5,10 @@
 package trainwatcher;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,15 +17,46 @@ import java.util.logging.Logger;
  * @author ovoloshchuk
  */
 public class TrainWatcher {
+    
+    static String request(String sUrl, HashMap<String, String> sCookies) throws IOException {
+        URL u = new URL(sUrl);
+        HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setInstanceFollowRedirects(false);
+        conn.connect();
+
+
+        String sHeaderName;
+        String sLocation = null;
+        for (int i = 1; (sHeaderName = conn.getHeaderFieldKey(i)) != null; i++) {
+            if (sHeaderName.equals("Set-Cookie")) {
+                String[] cookie = conn.getHeaderField(i).split("=");
+                if (cookie[1].startsWith("deleted")) {
+                    sCookies.remove(cookie[1]);
+                    continue;
+                }
+                sCookies.put(cookie[0], cookie[1]);
+            } else if (sHeaderName.equals("Location")){
+                sLocation = conn.getHeaderField(i);
+            }
+        }
+        return sLocation;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        TrainClient tc = new TrainClient("setekh", "qd4Q2eGF");
         try {
-            System.out.println(tc.login());
-            tc.getCities("Львів");
+            HashMap<String, String> sCookies = new HashMap<String, String>();
+            String sLocation = request("http://localhost/test/index.php", sCookies);
+            if (sLocation != null) {
+                sLocation = request(sLocation, sCookies);
+            }
+            
+            for (String sKey : sCookies.keySet()) {
+                System.out.println(String.format("%s: %s", sKey, sCookies.get(sKey)));
+            }
         } catch (IOException ex) {
             Logger.getLogger(TrainWatcher.class.getName()).log(Level.SEVERE, null, ex);
         }
